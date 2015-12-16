6ff5b200291dd72ded4705f3a2d7e3f310ac3c17
Adam Allgood <allgoodadam@gmail.com>
2015-12-16 11:13:24 -0800

    added ideas controller

---
+++ client/js/app/ideas.controller.js
@@ -0,0 +1,27 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('ideabank')
+    .controller('IdeasController', IdeasController);
+
+  IdeasController.$inject = ['$http'];
+
+  function IdeasController($http){
+    var vm = this;
+
+    vm.ideas = [];
+    vm.newIdea = {};
+
+    vm.submitIdea = submitIdea;
+
+    function submitIdea() {
+      $http
+        .post("http://localhost3000/ideas", vm.newIdea)
+        .then(function (response) {
+          vm.ideas.push(response.data);
+          vm.newIdea = {};
+        });
+    }
+  }
+})();
