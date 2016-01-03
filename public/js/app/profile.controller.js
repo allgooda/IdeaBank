e63388e49d5fb9ff9bba73fcca1952d8f98208e7
Adam Allgood <allgoodadam@gmail.com>
2016-01-03 14:03:27 -0800

    did some styling on the graph, it populates data per the user

--- public/js/app/profile.controller.js
+++ public/js/app/profile.controller.js
@@ -11,6 +11,67 @@
 
     $scope.labels = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "Novemeber", "December"];
 
+    $scope.options =  {
+
+      // Sets the chart to be responsive
+      responsive: true,
+
+      //Boolean - Whether to show lines for each scale point
+      scaleShowLine : true,
+
+      //Boolean - Whether we show the angle lines out of the radar
+      angleShowLineOut : true,
+
+      //Boolean - Whether to show labels on the scale
+      scaleShowLabels : false,
+
+      // Boolean - Whether the scale should begin at zero
+      scaleBeginAtZero : true,
+
+      //String - Colour of the angle line
+      angleLineColor : 'rgba(0,0,0,.9)',
+
+      //Number - Pixel width of the angle line
+      angleLineWidth : 1,
+
+      //String - Point label font declaration
+      pointLabelFontFamily : '"Arial"',
+
+      //String - Point label font weight
+      pointLabelFontStyle : 'bold',
+
+      //Number - Point label font size in pixels
+      pointLabelFontSize : 18,
+
+      //String - Point label font colour
+      pointLabelFontColor : 'black',
+
+      //Boolean - Whether to show a dot for each point
+      pointDot : true,
+
+      //Number - Radius of each point dot in pixels
+      pointDotRadius : 5,
+
+      //Number - Pixel width of point dot stroke
+      pointDotStrokeWidth : 1,
+
+      //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+      pointHitDetectionRadius : 20,
+
+      //Boolean - Whether to show a stroke for datasets
+      datasetStroke : false,
+
+      //Number - Pixel width of dataset stroke
+      datasetStrokeWidth : 2,
+
+      //Boolean - Whether to fill the dataset with a colour
+      datasetFill : true,
+
+      //String - A legend template
+      legendTemplate : '<ul class="tc-chart-js-legend"><% for (var i=0; i<datasets.length; i++){%><li><span style="background-color:<%=datasets[i].strokeColor%>"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>'
+    };
+
+
     $http
       .get("http://localhost:3000/profile")
       .then(function (response) {
