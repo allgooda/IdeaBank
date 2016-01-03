610c9a8265948cab90b56669a50f15e9f2f8d258
Adam Allgood <allgoodadam@gmail.com>
2016-01-03 13:20:43 -0800

    finished function to populate graph data

--- public/js/app/profile.controller.js
+++ public/js/app/profile.controller.js
@@ -29,11 +29,42 @@
         console.log(month);
         if(month === 0){
           amounts[0] = 1 + amounts[0];
-          console.log(amounts);
         }
-      amounts[5] = 10;
-      return amounts;
+        else if(month === 1){
+          amounts[1] = 1 + amounts[1];
+        }
+        else if(month === 2){
+          amounts[2] = 1 + amounts[2];
+        }
+        else if(month === 3){
+          amounts[3] = 1 + amounts[3];
+        }
+        else if(month === 4){
+          amounts[4] = 1 + amounts[4];
+        }
+        else if(month === 5){
+          amounts[5] = 1 + amounts[5];
+        }
+        else if(month === 6){
+          amounts[6] = 1 + amounts[6];
+        }
+        else if(month === 7){
+          amounts[7] = 1 + amounts[7];
+        }
+        else if(month === 8){
+          amounts[8] = 1 + amounts[8];
+        }
+        else if(month === 9){
+          amounts[9] = 1 + amounts[9];
+        }
+        else if(month === 10){
+          amounts[10] = 1 + amounts[10];
+        }
+        else if(month === 11){
+          amounts[11] = 1 + amounts[11];
+        }
       }
+      return amounts;
     }
   }
 })();
