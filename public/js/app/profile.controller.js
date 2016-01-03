4c835e44d985c0b810c0f70cb5be449a6297941c
Adam Allgood <allgoodadam@gmail.com>
2016-01-03 12:55:25 -0800

    still trying to put data in angular chart. submited stack overflow question

--- public/js/app/profile.controller.js
+++ public/js/app/profile.controller.js
@@ -8,25 +8,36 @@
   ProfileController.$inject = ['$http', '$scope'];
 
   function ProfileController($http, $scope) {
-    // var vm = this;
 
     $scope.labels = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "Novemeber", "December"];
-    $scope.data = [
-    [65, 59, 90, 81, 56, 55, 40, 28, 48, 40, 19],
-  ];
-    $scope.profileInfo = [];
 
   $scope.getProfile = getProfile;
 
     function getProfile() {
       $http
-        .get("http://localhost:3000/profile", $scope.profileInfo)
+        .get("http://localhost:3000/profile")
         .then(function (response) {
-          console.log(response.data);
-          console.log($scope.profileInfo);
-          // $scope.profileInfo = response.data
-
+          var profileData = populateChart(response.data.pastIdeas);
+          $scope.data = [];
+          $scope.data.push(profileData);
+          console.log($scope.data);
         });
+    }
+
+    function populateChart(pastIdeas) {
+      console.log(pastIdeas);
+      var amounts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+      for(var i=0; i < pastIdeas.length; i++) {
+        var ideaDate = new Date(pastIdeas[i].date);
+        var month = ideaDate.getMonth();
+        console.log(month);
+        if(month === 0){
+          amounts[0] = 1 + amounts[0];
+          console.log(amounts);
+        }
+      amounts[5] = 10;
+      return amounts;
+      }
     }
   }
 })();
