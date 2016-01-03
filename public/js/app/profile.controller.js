0e84ff2f24bc6c4ac96ee33e9deaccfb7f3f207a
Adam Allgood <allgoodadam@gmail.com>
2016-01-02 19:49:48 -0800

    trying to populate chart with the data of specific user

--- public/js/app/profile.controller.js
+++ public/js/app/profile.controller.js
@@ -8,25 +8,34 @@
   ProfileController.$inject = ['$http', '$scope'];
 
   function ProfileController($http, $scope) {
-    // var vm = this;
 
     $scope.labels = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "Novemeber", "December"];
-    $scope.data = [
-    [65, 59, 90, 81, 56, 55, 40, 28, 48, 40, 19],
-  ];
-    $scope.profileInfo = [];
 
-  $scope.getProfile = getProfile;
+    $scope.getProfile = getProfile;
+    $scope.populateChart = populateChart;
 
     function getProfile() {
       $http
         .get("http://localhost:3000/profile", $scope.profileInfo)
         .then(function (response) {
-          console.log(response.data);
-          console.log($scope.profileInfo);
-          // $scope.profileInfo = response.data
-
+          populateChart(response.data.pastIdeas);
         });
     }
+
+    function populateChart(pastIdeas) {
+      console.log(pastIdeas);
+      var amounts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+      for(var i=0; i < pastIdeas.length; i++) {
+        var ideaDate = new Date(pastIdeas[i].date);
+        var month = ideaDate.getMonth();
+        console.log(month);
+        if(month === 0){
+          amounts[0] = 1 + amounts[0];
+          console.log(amounts);
+        }
+      return amounts;
+      }
+    }
+
   }
 })();
